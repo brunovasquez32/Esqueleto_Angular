7d7651a320b0f9361751ca7c9b507de502d8d01c
Bruno Vásquez <bruno.vasquez.guerra@cftmail.cl>
2024-10-10 11:50:25 -0300

    Esqueleto para trabajar en Angular

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,21 @@
 import { Routes } from '@angular/router';
+import { ContactoComponent } from './componentes/contacto/contacto.component';
+import { InicioComponent } from './componentes/inicio/inicio.component';
+import { RegistrateComponent } from './componentes/registrate/registrate.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path:"contactanos",
+        component:ContactoComponent
+    },
+
+    {
+        path:"inicio",
+        component:InicioComponent
+    },
+    
+    {
+        path:"registro",
+        component:RegistrateComponent
+    }
+];
